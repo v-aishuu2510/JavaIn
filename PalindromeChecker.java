53826fe4a579de98ce231ccf0a372df476b0a346
Vaishali Raut <142431370+v-aishuu2510@users.noreply.github.com>
2024-03-25 12:14:02 +0530

    Update PalindromeChecker.java

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -8,25 +8,35 @@ public class PalindromeChecker
 	public static boolean isPalindrome(String str)
 	{
 		
-		String text1=str.replaceAll("[^a-zA-Z]", "").toLowerCase();
-		String text2=new StringBuilder(text1).reverse().toString();
+		String text=str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 		
-		return text1.equals(text2);
+		int leftside=0;
+		int rightside=text.length()-1;
+		
+		while(leftside < rightside )
+		{
+			if(str.charAt(leftside)==str.charAt(rightside))
+			{
+				return true;
+			}
+			leftside++;
+			rightside--;
+		}
+		return false;
 	}
 	static Scanner sc=new Scanner(System.in);
 	public static void main(String args[])
 	{
 		System.out.print("Enter the Word or phrase: ");
-		String text=sc.next();
-		
+		String text=sc.nextLine();	
 		System.out.println();
 		if(isPalindrome(text))
 		{
-			System.out.println(text +" is a Palindorme.");
+			System.out.println(" \" "+text +" \"  is a Palindorme.");
 		}
 		else
 		{
-			System.out.println(text +" is not Palindrome.");
+			System.out.println("\" "+text +" \" is not Palindrome.");
 		}
 	}
 }
